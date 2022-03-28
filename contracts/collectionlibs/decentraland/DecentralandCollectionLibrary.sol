349708cb10c36e2f5c5efb1dbccb2d622f6012aa
Emiliano Bonassi <bonassi.emiliano@gmail.com>
2022-03-28 17:12:25 +0200

    fix

--- contracts/collectionlibs/decentraland/DecentralandCollectionLibrary.sol
+++ contracts/collectionlibs/decentraland/DecentralandCollectionLibrary.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.13;
 
 import "../ICollectionLibrary.sol";
 import "./ILandRegistry.sol";
-import "../../IRentable.sol";
 
 contract DecentralandCollectionLibrary is ICollectionLibrary {
     function postDeposit(
