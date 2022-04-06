f80fa50d6c9a630468a69476ed702a44db7e8ba8
Emiliano Bonassi <bonassi.emiliano@gmail.com>
2022-04-06 16:07:17 +0200

    docs(collections): initial docs

--- contracts/collections/decentraland/DecentralandCollectionLibrary.sol
+++ contracts/collections/decentraland/DecentralandCollectionLibrary.sol
@@ -2,18 +2,27 @@
 
 pragma solidity ^0.8.13;
 
+// Inheritance
 import {ICollectionLibrary} from "../ICollectionLibrary.sol";
+
+// References
 import {ILandRegistry} from "./ILandRegistry.sol";
 
+/// @title Decentraland LAND collection library
+/// @author Rentable Team <hello@rentable.world>
+/// @notice Implement dedicated logic for LAND rentals
 contract DecentralandCollectionLibrary is ICollectionLibrary {
+    /// @inheritdoc ICollectionLibrary
     function postDeposit(
         address tokenAddress,
         uint256 tokenId,
         address user
     ) public {
+        // Depositor can continue to manage land operators after deposit
         ILandRegistry(tokenAddress).setUpdateOperator(tokenId, user);
     }
 
+    /// @inheritdoc ICollectionLibrary
     function postList(
         address,
         uint256,
@@ -22,6 +31,7 @@ contract DecentralandCollectionLibrary is ICollectionLibrary {
         uint256
     ) public {}
 
+    /// @inheritdoc ICollectionLibrary
     function postRent(
         address tokenAddress,
         uint256 tokenId,
@@ -29,26 +39,32 @@ contract DecentralandCollectionLibrary is ICollectionLibrary {
         address,
         address to
     ) public payable {
+        // Set renter as land operator
         ILandRegistry(tokenAddress).setUpdateOperator(tokenId, to);
     }
 
+    /// @inheritdoc ICollectionLibrary
     function postExpireRental(
         address tokenAddress,
         uint256 tokenId,
         address from
     ) external payable {
+        // Restore current otoken owner as land operator
         ILandRegistry(tokenAddress).setUpdateOperator(tokenId, from);
     }
 
+    /// @inheritdoc ICollectionLibrary
     function postWTokenTransfer(
         address tokenAddress,
         uint256 tokenId,
         address,
         address to
     ) external {
+        // Enable subletting, renter can transfer the right to update a land
         ILandRegistry(tokenAddress).setUpdateOperator(tokenId, to);
     }
 
+    /// @inheritdoc ICollectionLibrary
     function postOTokenTransfer(
         address tokenAddress,
         uint256 tokenId,
@@ -56,6 +72,7 @@ contract DecentralandCollectionLibrary is ICollectionLibrary {
         address to,
         bool rented
     ) external {
+        // Depositor can transfer the right to update a land when not rented out
         if (!rented) ILandRegistry(tokenAddress).setUpdateOperator(tokenId, to);
     }
 }
--- contracts/collections/decentraland/OLandRegistry.sol
+++ contracts/collections/decentraland/OLandRegistry.sol
@@ -2,19 +2,36 @@
 
 pragma solidity ^0.8.13;
 
+// Inheritance
+import {ORentable} from "../../tokenization/ORentable.sol";
+
+// References
 import {IRentable} from "../../interfaces/IRentable.sol";
 import {IORentableHooks} from "../../interfaces/IORentableHooks.sol";
 import {ILandRegistry} from "./ILandRegistry.sol";
-import {ORentable} from "../../tokenization/ORentable.sol";
 
+/// @title OToken for Decentraland LAND
+/// @author Rentable Team <hello@rentable.world>
+/// @notice Represents a transferrable tokenized deposit and mimics the wrapped token
 contract OLandRegistry is ORentable {
+    /* ========== CONSTRUCTOR ========== */
+
+    /// @notice Instantiate a token
+    /// @param wrapped wrapped token address
+    /// @param owner admin for the contract
+    /// @param rentable rentable address
     constructor(
-        address wrapped_,
+        address wrapped,
         address owner,
         address rentable
-    ) ORentable(wrapped_, owner, rentable) {}
+    ) ORentable(wrapped, owner, rentable) {}
 
+    /// @notice Update current land operator, who can update content
+    /// @param tokenId land identifier
+    /// @param operator operator address
     function setUpdateOperator(uint256 tokenId, address operator) external {
+        /// Makes operator updatable from the wrapper by depositor
+        /// So depositor can still do OTC rent when not rented via Rentable
         require(ownerOf(tokenId) == msg.sender, "User not allowed");
 
         require(
