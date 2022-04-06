596f10d4344ac2988e5375425ecd4a2071076b96
Emiliano Bonassi <bonassi.emiliano@gmail.com>
2022-04-06 11:33:08 +0200

    docs(rentable): rentable interfaces

--- contracts/interfaces/IRentableHooks.sol
+++ contracts/interfaces/IRentableHooks.sol
@@ -2,7 +2,16 @@
 
 pragma solidity ^0.8.13;
 
+/// @title Rentable Shared Hooks
+/// @author Rentable Team <hello@rentable.world>
 interface IRentableHooks {
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    /// @dev Implementer will execute the call on the wrapped token
+    /// @param to wrapped token address
+    /// @param value (optional) ether vaule
+    /// @param selector function selector on the target
+    /// @param data function data
     function proxyCall(
         address to,
         uint256 value,
